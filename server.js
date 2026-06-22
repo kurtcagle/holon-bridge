759dcee9a7ab3c9281c04471939d24b9b2ea871a
Kurt Cagle <kurt.cagle@gmail.com>
2026-06-22 14:13:31 -0700

    fix: add Authorization to CORS Allow-Headers for web client POST support

--- server.js
+++ server.js
@@ -740,7 +740,7 @@ app.use(express.json({ limit: '10mb' }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin',  '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   if (req.method === 'OPTIONS') return res.sendStatus(204)
   next()
